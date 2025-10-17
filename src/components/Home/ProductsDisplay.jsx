d4601544a1cb1066d0a969e1a490e6026e91d8c8
Shadaf Hossain <shadafhossain01@gmail.com>
2025-10-18 00:35:39 +0600

    Products Display done on Home Page

---
+++ src/components/Home/ProductsDisplay.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+const ProductsDisplay = () => {
+
+    const data=useSelector(state=>state.cart)
+
+  return (
+    <div className="grid grid-cols-2 gap-x-7">
+      {data.map((product) => (
+        <div
+          className="card bg-base-100 w-96 shadow-sm my-[30px]"
+          key={product.id}
+        >
+          <figure>
+            <img
+              src={product.image}
+              alt={product.name}
+              className="w-[384px] h-[200px] object-cover"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{product.name}</h2>
+            <p>{product.desc}</p>
+            <div className="card-actions justify-between items-center">
+              <button className="font-bold text-indigo-600 text-[20px] ">
+                ${product.price}
+              </button>
+              <button className="btn btn-primary mt-[5px]">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductsDisplay;
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,18 @@ import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Cart from './pages/Cart/Cart.jsx';
 import Home from './pages/Home/Home.jsx';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} >
-      <Route path='/' element={<Home/>} />
-      <Route path='/cart' element={<Cart/>}/>
-     </Route>
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
