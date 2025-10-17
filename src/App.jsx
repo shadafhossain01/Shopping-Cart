e94e481be39cb230e840c7d980b1c8138c718a5a
Shadaf Hossain <shadafhossain01@gmail.com>
2025-10-17 23:41:23 +0600

    Router Setup Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/common/Navbar";
+
 function App() {
 
   return (
     <>
-
+      <Navbar />
+      <div className="max-w-[1200px] mx-auto">
+        <Outlet />
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './pages/Cart/Cart.jsx';
+import Home from './pages/Home/Home.jsx';
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+      <Route path='/' element={<Home/>} />
+      <Route path='/cart' element={<Cart/>}/>
+     </Route>
+    </Routes>
+  </BrowserRouter>
+);
