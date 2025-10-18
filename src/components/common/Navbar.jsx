49b01a95f86877890b255a38bf1bb635b0fd2923
Shadaf Hossain <shadafhossain01@gmail.com>
2025-10-18 22:51:47 +0600

    Navbar Done

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const cartItem=useSelector(state=>state.cart)
+  const totalPrice=cartItem.reduce((total,eachItem)=>total+eachItem.price*eachItem.quantity,0)
+
   return (
     <div className="navbar shadow-sm py-5 bg-indigo-800 ">
       <div className="mx-auto w-[1200px] flex justify-between items-center ">
         <div className="flex-1">
-          <Link to="/" className=" text-[24px] text-white font-bold">Shopping Cart</Link>
+          <Link to="/" className=" text-[24px] text-white font-bold">
+            Shopping Cart
+          </Link>
         </div>
         <div className="flex-none">
           <div className="dropdown dropdown-end mr-[15px]">
-            <Link to="/" className="text-white font-semibold">Home</Link>
+            <Link to="/" className="text-white font-semibold">
+              Home
+            </Link>
           </div>
           <div className="dropdown dropdown-end mr-[15px]">
-            <Link to="/cart" className="text-white font-semibold">Cart</Link>
+            <Link to="/cart" className="text-white font-semibold">
+              Cart
+            </Link>
           </div>
           <div className="dropdown dropdown-end">
             <div
@@ -37,7 +48,9 @@ const Navbar = () => {
                     d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                   />{" "}
                 </svg>
-                <span className="badge badge-sm indicator-item">8</span>
+                <span className="badge badge-sm indicator-item">
+                  {cartItem.length}
+                </span>
               </div>
             </div>
             <div
@@ -45,12 +58,16 @@ const Navbar = () => {
               className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
             >
               <div className="card-body">
-                <span className="text-lg font-bold">8 Items</span>
-                <span className="text-info">Subtotal: $999</span>
+                <span className="text-lg font-bold">
+                  {cartItem.length} Items
+                </span>
+                <span className="text-info">Subtotal: ${totalPrice}</span>
                 <div className="card-actions">
-                  <button className="btn btn-primary btn-block">
-                    View cart
-                  </button>
+                  <Link to="/cart">
+                    <button className="btn btn-primary btn-block">
+                      View cart
+                    </button>
+                  </Link>
                 </div>
               </div>
             </div>
